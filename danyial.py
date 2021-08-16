c0ecb3f14b18cf8f2720445debb93050062e29ea
Arshadiqball <84665807+Arshadiqball@users.noreply.github.com>
2021-08-16 13:04:09 +0500

    Abu

---
+++ danyial.py
@@ -0,0 +1,60 @@
+import sublime
+import sublime_plugin
+
+
+class Laravel2Command(sublime_plugin.TextCommand):
+
+	def gog(self,edit,key):
+		print(key + " is pressed");
+		# print(fname + " Refsnes")
+		# self.view.insert(edit, 0, "Hello, 3!")
+		files=self.view.file_name().split("\\");
+		module_name=files[6];
+		print(files[7] + " is route");
+		if files[7]=='modules': 
+			files[6]=files[8];
+			pass
+		txt="";
+		if key=='v': 
+			txt="Modules\\"+files[6]+"\\Resources\\ blade.php";
+			pass
+		if key=='m': 
+			txt="Modules\\"+files[6]+"\\Entities\\ .php";
+			pass
+		if key=='w': 
+			txt="Modules\\"+files[6]+"\\Routes\\ .php";
+			pass
+		if key=='c': 
+			txt="Modules\\"+files[6]+"\\http\\ controller.php";
+			pass
+		if key=='j': 
+			txt="Modules\\"+files[6]+"\\http\\ controller.php";
+			pass
+
+
+
+		# if files[7]=='Resources': 
+		# 	txt="Modules\\"+files[6]+"\\http\\ controller.php";
+		# 	pass
+		# if files[7]=='Entities': 
+		# 	txt="Modules\\"+files[6]+"\\http\\ controller.php";
+		# 	pass
+		# if files[7]=='modules': 
+		# 	txt="Modules\\"+files[6]+"\\http\\ controller.php";
+		# 	pass
+		# if files[7]=='Http': 
+		# 	txt="Modules\\"+files[6]+"\\http\\ controller.php";
+		# 	pass
+		# 
+
+
+
+		self.view.window().run_command('show_overlay', {"overlay": "goto", "text": txt,"show_files": "true"})
+
+	 
+	def run(self, edit,key):
+		
+		self.gog(edit,key);
+
+
+
